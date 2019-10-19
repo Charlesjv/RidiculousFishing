520670a0fc008c5c979bdf58b447b2650b4fff8f
Charlesjv <charles764jaison@gmail.com>
2019-10-19 08:41:35 -0400

    Initializing the sprite Nodes

--- RidiculousFishing/GameScene.swift
+++ RidiculousFishing/GameScene.swift
@@ -10,4 +10,42 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    
+    
+    var fishCam: SKCameraNode?
+    var boat: SKSpriteNode?
+    var fisherman: SKSpriteNode?
+    var fishingpole: SKSpriteNode?
+    var hook: SKSpriteNode?
+    var fishingline: SKSpriteNode?
+    var pole:SKSpriteNode?
+    var line: SKSpriteNode?
+  
+
+    
+    var score: Int = 0
+    var lives: Int = 3
+    // Labels
+    var scoreLabel: SKLabelNode?
+    
+    
+    override func didMove(to view: SKView) {
+        sceneSetup()
+    }
+    
+    func sceneSetup(){
+        
+        fishCam = childNode(withName: "fishCam") as? SKCameraNode
+        camera = fishCam
+        
+        boat = childNode(withName: "boat") as? SKSpriteNode
+        fisherman = boat?.childNode(withName: "fisherman") as? SKSpriteNode
+        pole = boat?.childNode(withName: "pole") as? SKSpriteNode
+        line = childNode(withName: "line") as? SKSpriteNode
+        hook = line?.childNode(withName: "hook") as? SKSpriteNode
+        
+        
+    }
+   
 }
